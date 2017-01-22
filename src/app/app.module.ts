ee04a749543350a93a396e13c6fd6240302a84a5
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-01-22 22:39:38 +0100

    NavBar and router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 import './rxjs-extensions';
 
+import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
